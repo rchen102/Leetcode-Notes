40a6af02f16f60d1c8e49fd6ba6ca5733a2505e5
rchen102 <rchen102@binghamton.edu>
2018-05-07 22:08:06 +0800

    2018/5/7

---
+++ leetcode 122.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int sum = 0, diff = 0;
+        for(int i = 1; i < prices.length; i++) {
+            if((diff = prices[i] - prices[i-1]) > 0)
+                sum += diff;
+        }
+        return sum;
+    }
+}
