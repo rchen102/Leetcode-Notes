7bbd55b9c72e1aa8e90964fce24a9475c6e77106
rchen102 <rchen102@binghamton.edu>
2018-05-27 22:25:19 +0800

    2018/5/27

---
+++ leetcode 137.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int low = 0, high = 0;
+        for(int num : nums) {
+            low = (low ^ num) & ~high;
+            high = (high ^ num) & ~low;
+        }
+        return low;
+    }
+}
