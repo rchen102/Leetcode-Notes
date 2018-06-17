c144c5e423da8a8509839f16a02e9baf4d501765
rchen102 <rchen102@binghamton.edu>
2018-06-17 21:41:40 +0800

    2018/6/17

--- leetcode 85.java
+++ leetcode 85.java
@@ -39,3 +39,41 @@ class Solution {
         return res;
     }
 }
+
+//Solution2: using Largest Rectangle in Histogram  T: O(mn) S: O(max(rows,cols))
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[] height = new int[cols];
+        int res = 0;
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {  //compute height
+                if(matrix[i][j] == '1') height[j]++;
+                else height[j] = 0;
+            }
+            res = Math.max(res, maxArea(height));
+        }
+        return res;
+    }
+
+   private int maxArea(int[] heights) {
+        if(heights == null || heights.length == 0) return 0;
+        Stack<Integer> stack = new Stack<>();
+        int res = 0;
+        
+        for(int i = 0; i <= heights.length; i++) {
+            int h = (i == heights.length ? 0 : heights[i]);
+            while(!stack.isEmpty() && h < heights[stack.peek()]) {
+                int height = heights[stack.pop()];
+                int start = (stack.isEmpty() ? -1 : stack.peek());
+                int area = height * (i - start - 1);
+                res = Math.max(res, area);
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
