a16bf3ab643632fd64481da94084bbe54a37c63a
rchen102 <rchen102@binghamton.edu>
2018-07-20 23:35:57 +0800

    2018/7/20

---
+++ leetcode 105.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+//Own solution: recursive T: O(n^2)(worst) O(nlgn)(best) S: O(n)
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int length = preorder.length;
+        if(length < 1) return null;
+        return helper(preorder, inorder, 0, length-1, 0, length-1);
+    }
+    private TreeNode helper(int[] preorder, int[] inorder, int i, int j, int m, int n) {
+        if(i > j) return null;
+        if(i == j) {
+            TreeNode p = new TreeNode(preorder[j]);
+            return p;
+        }
+        int num = 0;
+        for(int k = m; k <= n; k++) {
+            if(inorder[k] == preorder[i]) {
+                num = k;
+            }
+        }
+        TreeNode root = new TreeNode(preorder[i]);
+        root.left = helper(preorder, inorder, i+1, i+num-m, m, num-1);
+        root.right = helper(preorder, inorder, i+num-m+1, j, num+1, n);
+        return root;
+    }
+}
