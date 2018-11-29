e6c33c465f7672d8a65050bd4c33e98216b6b47c
rchen102 <rchen102@binghamton.edu>
2018-11-28 22:52:16 -0500

    2018/11/28

---
+++ leetcode 876.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+// Own solution: fast & slow T: O(n) S: O(1)
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return fast.next != null ? slow.next : slow;
+    }
+}
