98882acdfe1c966ca67724144c8dd8ecb36a54fe
rchen102 <rchen102@binghamton.edu>
2018-12-29 20:22:59 -0500

    2018/12/29

--- leetcode 109.java
+++ leetcode 109.java
@@ -16,67 +16,33 @@
  * }
  */
 
-//Solution1: T: O(nlogn) S: O(logn)
+// Solution1: divide and conquer T: O(nlogn) S: O(logn)
 class Solution {
     public TreeNode sortedListToBST(ListNode head) {
-        if(head == null) return null;
-        
-        ListNode slow = head, fast = head, p = null;
-        
-        while(fast.next != null && fast.next.next != null) {
-            p = slow;
+        if (head == null) return null;
+        // Find mid
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode pre = null;
+        while (fast.next != null && fast.next.next != null) {
+            pre = slow;
             slow = slow.next;
             fast = fast.next.next;
         }
-        if(p != null) p.next = null;
-        
-        TreeNode res = new TreeNode(slow.val);
-        if(head != slow) res.left = sortedListToBST(head);
-        res.right = sortedListToBST(slow.next);
-        
-        return res;
-    }
-}
-
-
-//Own solution: divide and conquer T: O(nlogn) S: O(logn)
-class Solution {
-    public TreeNode sortedListToBST(ListNode head) {
-        int length = getLength(head);
-        if(length == 0) {
-            TreeNode p = null;
-            return p;
+        // Decide left and right
+        ListNode left;
+        ListNode right;
+        if (pre != null) {
+            pre.next = null;
+            left = head;
+        } else {
+            left = null;
         }
-        if(length == 1) {
-            TreeNode p = new TreeNode(head.val);
-            return p;
-        }
-        
-        int rootNum = length / 2;
-        ListNode p = head;
-        ListNode left, root, right;
-        
-        for(int i = 0; i < rootNum - 1; i++) {
-            p = p.next;
-        }
-        root = p.next;
-        p.next = null;
-        left = head;
-        right = root.next;
-        
-        TreeNode res = new TreeNode(root.val);
-        res.left = sortedListToBST(left);
-        res.right = sortedListToBST(right);
-        
-        return res;
-    }
-    
-    private int getLength(ListNode head) {
-        int length = 0;
-        while(head != null) {
-            length++;
-            head = head.next;
-        }
-        return length;
+        right = slow.next;
+        // Creat tree
+        TreeNode root = new TreeNode(slow.val);
+        root.left = sortedListToBST(left);
+        root.right = sortedListToBST(right);
+        return root;
     }
 }
--- leetcode 83.java
+++ leetcode 83.java
@@ -6,36 +6,22 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+// Solution1: iterative T: O(n) S: O(1)
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode p = head;
-        while(p != null) {
-            if(p.next == null) break;
-            if(p.val == p.next.val)
-                p.next = p.next.next;
-            else
-                p = p.next;
+        if (head == null || head.next == null) return head;  
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        while (head.next != null) {
+            if (head.val == head.next.val) {
+                head.next = head.next.next;
+            } else head = head.next;
         }
-        return head;
+        return dummy.next;
     }
 }
 
-//Solution2 : recursive
-
-class Solution {
-    public ListNode deleteDuplicates(ListNode head) {
-        if(head == null || head.next == null)return head;  
-        if(head.val != head.next.val)
-            head.next = deleteDuplicates(head.next);
-        else {
-            head = head.next;
-            head = deleteDuplicates(head);
-        }
-        return head;
-    }
-}
-
-//Slotion3 : recursive
+// Solution2: recursive T: O(n) S: O(n)
 public ListNode deleteDuplicates(ListNode head) {
         if(head == null || head.next == null) return head;
         head.next = deleteDuplicates(head.next);
