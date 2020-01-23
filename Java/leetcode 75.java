0fe1ee944c6b13db4886e783705d459552006c32
rchen102 <rchen102@binghamton.edu>
2020-01-22 23:28:34 -0500

    Array

--- Java/leetcode 75.java
+++ Java/leetcode 75.java
@@ -3,24 +3,22 @@
  */
 class Solution {
     public void sortColors(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
+        if (nums == null || nums.length == 0) return;
+        int left = 0, right = nums.length - 1;
         int cur = 0;
         while (cur <= right) {
-            if (nums[cur] == 0) {
-                nums[cur] = nums[left];
-                nums[left] = 0;
-                left++;
-                cur++;
-                continue;
-            }
             if (nums[cur] == 2) {
                 nums[cur] = nums[right];
                 nums[right] = 2;
                 right--;
-                continue;
             }
-            cur++;
+            else if (nums[cur] == 0) {
+                nums[cur] = nums[left]; // we can guarantee nums[left] can not be 2
+                nums[left] = 0;
+                left++;
+                cur++;      
+            }
+            else cur++;
         }
     }
 }
--- Java/leetcode 80.java
+++ Java/leetcode 80.java
@@ -3,9 +3,10 @@
  */
 class Solution {
     public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
         int cur = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (i < 2 || nums[i] != nums[cur-2]) {
+            if (cur < 2 || nums[cur-2] != nums[i]) {
                 nums[cur] = nums[i];
                 cur++;
             }
