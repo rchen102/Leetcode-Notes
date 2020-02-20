a6c744f2a143f29faba328069a4e8d2902b80114
rchen102 <rchen102@binghamton.edu>
2020-02-19 20:18:05 -0500

    Hash: 2

--- Java/leetcode 36.java
+++ Java/leetcode 36.java
@@ -1,20 +1,21 @@
+// Solution: HashSet
+// T: O(81) S: O(27)
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        for(int i = 0; i < 9; i++){
+        for (int i = 0; i < 9; i++) {
             Set<Character> row = new HashSet<>();
-            Set<Character> column = new HashSet<>();
+            Set<Character> col = new HashSet<>();
             Set<Character> cube = new HashSet<>();
             
-            int rowIndex = i / 3 * 3;
-            int columnIndex = i % 3 * 3; 
+            int rowIdx = i / 3 * 3;  // the top left element's row/col idx of each cube
+            int colIdx = i % 3 * 3;
             
-            for(int j = 0; j < 9; j++) {
-                if(board[i][j]!='.' && !row.add(board[i][j])) return false;
-                if(board[j][i]!='.' && !column.add(board[j][i])) return false;
-                if(board[rowIndex + j/3][columnIndex + j%3] != '.' && !cube.add(board[rowIndex + j/3][columnIndex + j%3])) return false;
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.' && !row.add(board[i][j])) return false;
+                if (board[j][i] != '.' && !col.add(board[j][i])) return false;
+                if (board[rowIdx + j/3][colIdx + j%3] != '.' && !cube.add(board[rowIdx + j/3][colIdx + j%3])) return false;
             }
         }
         return true;
-            
     }
 }
--- Java/leetcode 49.java
+++ Java/leetcode 49.java
@@ -1,71 +1,45 @@
-//Solution1: T: O(n * klogk) S: O(n)
+// Solution1: HashMap + sort
+// T: O(n * klogk) S: O(n)
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if(strs.length == 0 || strs == null) return new ArrayList<List<String>>();
+        if (strs == null || strs.length == 0) return null;
         Map<String, List<String>> map = new HashMap<>();
-        for(String str : strs) {
-            char[] ca = str.toCharArray();
-            Arrays.sort(ca);
-            String tmp = String.valueOf(ca);
-            if(!map.containsKey(tmp))
-                map.put(tmp, new ArrayList<String>());
-            map.get(tmp).add(str);
-                
+        for (String str : strs) {
+            char[] chs = str.toCharArray();
+            Arrays.sort(chs);
+            String tag = String.valueOf(chs);
+            
+            if (!map.containsKey(tag)) {
+                map.put(tag, new ArrayList<>());
+            }
+            map.get(tag).add(str);
         }
         return new ArrayList<>(map.values());
     }
 }
 
-//Solution2: T: O(n * k) S: O(n)
+// Solution2: HashMap + encoding function 
+// T: O(n * k) S: O(n)
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if(strs.length == 0 || strs == null) return new ArrayList<List<String>>();
+        if (strs == null || strs.length == 0) return null;
         Map<String, List<String>> map = new HashMap<>();
-        for(String str : strs) {
-            String tmp = encode(str);
-            if(!map.containsKey(tmp))
-                map.put(tmp, new ArrayList<String>());
-            map.get(tmp).add(str);
-                
+        for (String str : strs) {
+            String tag = this.encode(str);
+            
+            if (!map.containsKey(tag)) {
+                map.put(tag, new ArrayList<>());
+            }
+            map.get(tag).add(str);
         }
         return new ArrayList<>(map.values());
     }
     
     private String encode(String str) {
-        char[] freq = new char[26];
-        for(char c : str.toCharArray())
-            freq[c-'a'] += 1;
-        return new String(freq);
-    }
-}
-
-//Own way: T: O(n^2 * k) S: O(n)
-class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<>();
-        int[] used = new int[strs.length];
-        for(int i = 0; i < strs.length; i++) {
-            if(used[i] == 1) continue;
-            List<String> tmp = new ArrayList<>();
-            tmp.add(strs[i]);
-
-            for(int j = i + 1; j < strs.length; j++) {
-                if(isAnagram(strs[i], strs[j])) {
-                    used[j] = 1;
-                    tmp.add(strs[j]);
-                }
-            }
-            res.add(tmp);
+        char[] code = new char[26];      // ASCLL = 0 by default
+        for (char ch : str.toCharArray()) {
+            code[ch-'a'] += 1;
         }
-        return res;
-    }
-    
-    private boolean isAnagram(String s, String t) {
-        int[] alphabet = new int[26];
-        for(int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
-        for(int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
-        for(int i : alphabet)
-            if(i != 0) return false;
-        return true;
+        return String.valueOf(code);
     }
 }
