6b4da6db8cd23cbc4c3278b07b1e977e1cf26e1f
rchen102 <rchen102@binghamton.edu>
2020-10-06 23:44:46 -0400

    daily update

--- Java/leetcode 42.java
+++ Java/leetcode 42.java
@@ -1,30 +1,46 @@
-//Solution1: two pointers T: O(n) S: O(1)
+// Solution：容易理解的暴力解法  T:O(n) S: O(n)
 class Solution {
     public int trap(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
-        int leftMax = 0, rightMax = 0;
-        
+        if (height == null || height.length == 0) return 0;
+        int n = height.length;
+        int[] l_max = new int[n]; 
+        int[] r_max = new int[n];
+        l_max[0] = height[0];
+        r_max[n-1] = height[n-1];
+        for (int i = 1; i < n; i++) {
+            l_max[i] = Math.max(height[i-1], l_max[i-1]);
+        }
+        for (int i = n-2; i >= 0; i--) {
+            r_max[i] = Math.max(height[i+1], r_max[i+1]);
+        }
         int res = 0;
-        // the solution is to calculate the trapped water at each index 
-        // And sum them together to get result
+        for (int i = 0; i < n; i++) {
+            int tall = Math.min(l_max[i], r_max[i]);
+            if (tall > height[i]) res += tall - height[i];
+        }
+        return res;
+    }
+}
 
-        while(left <= right) {
-            //leftMax: record the max height among A[0...left]
-            //rightMax: record the max height among A[right....length-1]
-            //when leftMax <= rightMax, at least (leftMax - A[left])water can be stored
-            //no matter what happens between [left..right]
-            //since we know there is a barrier at the rightside(rightMax>leftMax) 
-            leftMax = Math.max(leftMax, height[left]);
-            rightMax = Math.max(rightMax, height[right]);
-            if(leftMax <= rightMax) {
-                res += leftMax - height[left];  // leftmax is smaller than rightmax, so the (leftmax-A[left]) water can be stored
+// Solution：优化过后的双指针解法 T: O(n) S: O(1)
+class Solution {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) return 0;
+        int n = height.length;
+        int l_max = height[0], r_max = height[n-1];
+        int left = 0, right = n-1;
+        int res = 0;
+        while (left <= right) {
+            l_max = Math.max(l_max, height[left]);
+            r_max = Math.max(r_max, height[right]);
+            if (l_max < r_max) {
+                res += l_max - height[left];
                 left++;
             }
             else {
-                res += rightMax - height[right];
+                res += r_max - height[right];
                 right--;
-            }  
+            }
         }
         return res;
     }
